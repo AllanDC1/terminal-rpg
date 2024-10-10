f974cfc5b2c0f42af9fd7294edd77bbe7a7ef445
Allan Donetti Calen <162832747+AllanDC1@users.noreply.github.com>
2024-10-10 17:19:04 +0000

    Structs base criadas

--- terminalrpg.h
+++ terminalrpg.h
@@ -12,4 +12,46 @@
 #include <unistd.h>
 #endif
 
+typedef struct {
+    int vida;
+    int dano;
+} Status;
+
+typedef struct {
+    int ID;
+    char nome[255];
+    int quantidade;
+} Item;
+
+typedef struct {
+    int ID;
+    char nome[255];
+    int dano;
+} Habilidade;
+
+typedef struct {
+    char nome_usuario[20];
+    char senha[15];
+    char nickname[30];
+    double nivel;
+    Item consumiveis[5];
+    Habilidade inventario[10];
+    Status status;
+} Usuario;
+
+typedef struct {
+    int ID;
+    char nome[255];
+    int dificuldade;
+    int qnt_moedas;
+    Habilidade recompensas[5];
+} Dungeon;
+
+typedef struct {
+    int ID;
+    char nome[255];
+    Status status;
+    float qnt_xp;
+} Inimigo;
+
 #endif
